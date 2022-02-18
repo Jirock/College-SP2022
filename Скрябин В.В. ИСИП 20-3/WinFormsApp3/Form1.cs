1fab9044905ff22d73fdad96f8672fbcffa965d5
PushOk <70839406+Jirock@users.noreply.github.com>
2022-02-18 12:26:22 +0900

    LAB-2 2022

---
+++ Скрябин В.В. ИСИП 20-3/WinFormsApp3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            double z = Convert.ToDouble(textBox3.Text);
+            double result = (2 * Math.Cos(x - Math.PI / 6)) / (0.5 + Math.Pow(Math.Sin(y), 2)) * (1 + Math.Pow(z, 2) / (3 - Math.Pow(z, 2) / 5));
+
+            textBox4.Text += Environment.NewLine + "Выполнил: Скрябин В.В. ИСИП 20-3";
+            textBox4.Text += Environment.NewLine + "X = " + x.ToString();
+            textBox4.Text += Environment.NewLine + "Y = " + y.ToString();
+            textBox4.Text += Environment.NewLine + "Z = " + z.ToString();
+            textBox4.Text += Environment.NewLine + "Результат U = " + result.ToString();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
