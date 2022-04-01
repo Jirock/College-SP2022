392e914dd1f6b41c573c93cade8e9eb6240d75ce
PushOk <70839406+Jirock@users.noreply.github.com>
2022-04-01 11:17:40 +0900

    Lab9

---
+++ Скрябин В.В. ИСИП 20-3 Лаб 9/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxX0 = new System.Windows.Forms.TextBox();
+            this.textBoxXk = new System.Windows.Forms.TextBox();
+            this.textBoxDx = new System.Windows.Forms.TextBox();
+            this.X0 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(58, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(682, 336);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(484, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Расчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxX0
+            // 
+            this.textBoxX0.Location = new System.Drawing.Point(60, 403);
+            this.textBoxX0.Name = "textBoxX0";
+            this.textBoxX0.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX0.TabIndex = 1;
+            // 
+            // textBoxXk
+            // 
+            this.textBoxXk.Location = new System.Drawing.Point(166, 403);
+            this.textBoxXk.Name = "textBoxXk";
+            this.textBoxXk.Size = new System.Drawing.Size(100, 20);
+            this.textBoxXk.TabIndex = 2;
+            // 
+            // textBoxDx
+            // 
+            this.textBoxDx.Location = new System.Drawing.Point(272, 403);
+            this.textBoxDx.Name = "textBoxDx";
+            this.textBoxDx.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDx.TabIndex = 3;
+            // 
+            // X0
+            // 
+            this.X0.AutoSize = true;
+            this.X0.Location = new System.Drawing.Point(58, 384);
+            this.X0.Name = "X0";
+            this.X0.Size = new System.Drawing.Size(20, 13);
+            this.X0.TabIndex = 4;
+            this.X0.Text = "X0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Xk";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Dx";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(375, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.X0);
+            this.Controls.Add(this.textBoxDx);
+            this.Controls.Add(this.textBoxXk);
+            this.Controls.Add(this.textBoxX0);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxX0;
+        private System.Windows.Forms.TextBox textBoxXk;
+        private System.Windows.Forms.TextBox textBoxDx;
+        private System.Windows.Forms.Label X0;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Скрябин В.В. ИСИП 20-3 Лаб 9/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x0 = Convert.ToDouble(textBoxX0.Text);
+            double xk = Convert.ToDouble(textBoxXk.Text);
+            double dx = Convert.ToDouble(textBoxDx.Text);
+            double y = 0;
+            double x = x0;
+            while (x <= (xk + dx / 2))
+            {
+                double b = 2.5;
+                y = 9 * (x + 15 * Math.Sqrt(Math.Pow(x, 3) + Math.Pow(b, 3)));
+                x = x + dx;
+            }
+
+
+            int y1 = Convert.ToInt32(y);
+            double[] yX = new double[y1];
+            double[] Aard = new double[y1];
+
+            for (int i = 0; i < y1; i++)
+            {
+                yX[i] = xk + dx * i;
+                Aard[i] = Math.Sin(yX[i]);
+            }
+
+            // Настраиваем оси графика
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = dx;
+            // Определяем шаг сетки
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = xk;
+            // Добавляем вычисленные значения в графики
+            chart1.Series[0].Points.DataBindXY(yX,Aard);
+        }
+    }
+}
